1868ce41879c7f473a99b28809da3521190b2c51
ajspalma <palma.ashleyjs@gmail.com>
2020-09-15 15:00:40 +0800

    Added Enviroment Variable PORT (Custom) - needed in the heroku deployment

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db); });
 
 app.put('/imageurl', (req, res) => { image.handleApiCall(req, res); });
  
-app.listen(4000, () => {
-    console.log('app is running on port 4000');
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`app is running on port ${ process.env.PORT || 4000 }`);
 });
